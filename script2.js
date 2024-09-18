837468abb6056006e83e838e34935faccb3b24e5
Jen Chen <jenchen1177@yahoo.ca>
2024-09-18 15:40:49 -0400

    Added portfolio page

---
+++ script2.js
@@ -0,0 +1,66 @@
+
+/*
+// Get the elements by their ID
+var popupLink = document.getElementById("popup-link");
+var popupWindow = document.getElementById("popup-window");
+var closeButtonTop = document.getElementById("close-button-top");
+var closeButtonBottom = document.getElementById("close-button-bottom");
+
+popupLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  popupWindow.style.display = "block";
+});
+
+closeButtonTop.addEventListener("click", function() {
+  popupWindow.style.display = "none";
+});
+closeButtonBottom.addEventListener("click", function() {
+  popupWindow.style.display = "none";
+});
+*/
+
+
+function showPopupBowling() {
+  var popupWindow = document.getElementById("popup-window1");
+  var closeButtonTop = document.getElementById("close-button-top1");
+  var closeButtonBottom = document.getElementById("close-button-bottom1");
+
+  popupWindow.style.display = "block";
+
+  closeButtonTop.addEventListener("click", function() {
+    popupWindow.style.display = "none";
+  });
+  closeButtonBottom.addEventListener("click", function() {
+    popupWindow.style.display = "none";
+  });
+}
+
+function showPopupVC() {
+  var popupWindow = document.getElementById("popup-window2");
+  var closeButtonTop = document.getElementById("close-button-top2");
+  var closeButtonBottom = document.getElementById("close-button-bottom2");
+
+  popupWindow.style.display = "block";
+
+  closeButtonTop.addEventListener("click", function() {
+    popupWindow.style.display = "none";
+  });
+  closeButtonBottom.addEventListener("click", function() {
+    popupWindow.style.display = "none";
+  });
+}
+
+function showPopupJumpGuy() {
+  var popupWindow = document.getElementById("popup-window3");
+  var closeButtonTop = document.getElementById("close-button-top3");
+  var closeButtonBottom = document.getElementById("close-button-bottom3");
+
+  popupWindow.style.display = "block";
+
+  closeButtonTop.addEventListener("click", function() {
+    popupWindow.style.display = "none";
+  });
+  closeButtonBottom.addEventListener("click", function() {
+    popupWindow.style.display = "none";
+  });
+}
